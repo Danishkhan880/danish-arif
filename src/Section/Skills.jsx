58b9fd6af68bfb335d439ed6956b218bc8b94447
Danish Arif <162559282+Danishkhan880@users.noreply.github.com>
2026-06-19 18:06:13 +0500

    Update Skills.jsx

--- src/Section/Skills.jsx
+++ src/Section/Skills.jsx
@@ -8,10 +8,10 @@ import { IoLogoJavascript } from "react-icons/io";
 import { IoLogoCss3 } from "react-icons/io5";
 
 const skills = [
-  { logo: <FaReact />, title: "React JS", disc: "UI Library", color: "#61DAFB" },
+  { logo: <FaReact />, title: "Wordpress", disc: "UI Library", color: "#61DAFB" },
   { logo: <FaFigma />, title: "Figma", disc: "Design Tool", color: "#F24E1E" },
   { logo: <RiTailwindCssFill />, title: "TailWind", disc: "Utility-First CSS", color: "#06B6D4" },
-  { logo: <RiNextjsLine />, title: "Next JS", disc: "React Framework", color: "#FFFFFF" },
+  { logo: <RiNextjsLine />, title: "Seo", disc: "React Framework", color: "#FFFFFF" },
   { logo: <IoLogoJavascript />, title: "JavaScript", disc: "Client-side scripting", color: "#F7DF1E" },
   { logo: <FaGithub />, title: "Github", disc: "Code collaboration", color: "#E6EDF3" },
   { logo: <FaBootstrap />, title: "BootStrap", disc: "CSS Framework", color: "#7952B3" },
